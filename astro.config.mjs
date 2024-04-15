260008be7acf99e0c68959dc19289942e396c127
Dor Katzir <katzirdor@gmail.com>
2024-04-15 11:57:58 +0300

    relative path installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
+import relativeLinks from 'astro-relative-links';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [icon()],
+
+    integrations: [icon(), relativeLinks()],
+    outDir: './docs',
 });
